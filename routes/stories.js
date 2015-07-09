b99f2841579933c93e618d595ff78cdef67be8e9
diegoyep <diego.yepes@zefira.pe>
2015-07-08 21:06:32 -0500

    more changes

--- routes/stories.js
+++ routes/stories.js
@@ -4,4 +4,8 @@ module.exports = function(app){
 	app.get('/story', function(req, res, next){
 		res.render('story', {title: "story"});
 	});
+
+	app.get('/stories', function(req, res, next){
+		res.render('stories', {title:"stories"})
+	})
 }
